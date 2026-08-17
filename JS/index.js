d14a680e58e207c689d2f2b3d62d5e41a5e3d439
ashis <bwit24e.ais@ismt.edu.np>
2026-08-17 10:23:59 +0545

    data types console and operators

---
+++ JS/index.js
@@ -0,0 +1,47 @@
+//consoles
+console.log("Hello World");
+console.warn("This is a warning");
+console.error("This is an error");
+
+//variables
+let name = "Ashish";
+console.log(name);
+
+let a=10;
+let b=20;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log(a++);
+console.log(a--);
+console.log(++a);
+console.log(--a);
+console.log(a==b);
+console.log(a!=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a===b);
+console.log(a!==b);
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);
+console.log(!b);
+console.log(a&b);
+console.log(a|b);
+console.log(a^b);
+console.log(~a);
+console.log(~b);
+console.log(a<<b);
+console.log(a>>b);
+console.log(a>>>b);
+
+//data types
+
+let x = 10;
+console.log(typeof x);
