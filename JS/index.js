ef4b253802edb1d70ce3efba0764bce79c4ccb48
ashis <bwit24e.ais@ismt.edu.np>
2026-08-18 10:30:32 +0545

    DAY 2

--- JS/index.js
+++ JS/index.js
@@ -1,47 +1,217 @@
-//consoles
-console.log("Hello World");
-console.warn("This is a warning");
-console.error("This is an error");
+// //consoles
+// console.log("Hello World");
+// console.warn("This is a warning");
+// console.error("This is an error");
 
-//variables
-let name = "Ashish";
-console.log(name);
+// //variables
+// let name = "Ashish";
+// console.log(name);
 
-let a=10;
-let b=20;
+// let a=10;
+// let b=20;
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
-console.log(a**b);
-console.log(a++);
-console.log(a--);
-console.log(++a);
-console.log(--a);
-console.log(a==b);
-console.log(a!=b);
-console.log(a>b);
-console.log(a<b);
-console.log(a>=b);
-console.log(a<=b);
-console.log(a===b);
-console.log(a!==b);
-console.log(a&&b);
-console.log(a||b);
-console.log(!a);
-console.log(!b);
-console.log(a&b);
-console.log(a|b);
-console.log(a^b);
-console.log(~a);
-console.log(~b);
-console.log(a<<b);
-console.log(a>>b);
-console.log(a>>>b);
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// console.log(a++);
+// console.log(a--);
+// console.log(++a);
+// console.log(--a);
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a===b);
+// console.log(a!==b);
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
+// console.log(!b);
+// console.log(a&b);
+// console.log(a|b);
+// console.log(a^b);
+// console.log(~a);
+// console.log(~b);
+// console.log(a<<b);
+// console.log(a>>b);
+// console.log(a>>>b);
 
-//data types
+// //data types
+// let x = 10;
+// console.log(typeof x);
 
-let x = 10;
-console.log(typeof x);
+// //control statements
+
+
+
+
+// //Decision Making Statements
+// let aa = 10;
+// if(aa>10){
+//     console.log("a is greater than 10  ");
+// }
+// else if(aa<10){
+//     console.log("a is less than 10");
+// }
+// else if(aa==10){
+//     console.log("a is equal to 10");
+// }
+// else{
+//     console.log("a is smaller than 10  ");
+// }
+
+
+//abcde
+let a = 10;
+let b = 5;
+let c = 15;
+let d = 1;
+let e = 32;
+
+if (a >= b) {
+    if (a >= c) {
+        if (a >= d) {
+            if (a >= e) {
+
+                // a is greatest or equal to greatest
+                if (a == b) {
+                    if (a == c) {
+                        if (a == d) {
+                            if (a == e) {
+                                console.log("All numbers are equal");
+                            } else {
+                                console.log("a, b, c and d are equal and greatest");
+                            }
+                        } else {
+                            if (a == e) {
+                                console.log("a, b and e are equal and greatest");
+                            } else {
+                                console.log("a, b and c are equal and greatest");
+                            }
+                        }
+                    } else {
+                        if (a == d) {
+                            if (a == e) {
+                                console.log("a, b, d and e are equal and greatest");
+                            } else {
+                                console.log("a, b and d are equal and greatest");
+                            }
+                        } else {
+                            if (a == e) {
+                                console.log("a, b and e are equal and greatest");
+                            } else {
+                                console.log("a and b are equal and greatest");
+                            }
+                        }
+                    }
+                } else {
+                    if (a == c) {
+                        if (a == d) {
+                            if (a == e) {
+                                console.log("a, c, d and e are equal and greatest");
+                            } else {
+                                console.log("a, c and d are equal and greatest");
+                            }
+                        } else {
+                            if (a == e) {
+                                console.log("a, c and e are equal and greatest");
+                            } else {
+                                console.log("a and c are equal and greatest");
+                            }
+                        }
+                    } else {
+                        if (a == d) {
+                            if (a == e) {
+                                console.log("a, d and e are equal and greatest");
+                            } else {
+                                console.log("a and d are equal and greatest");
+                            }
+                        } else {
+                            if (a == e) {
+                                console.log("a and e are equal and greatest");
+                            } else {
+                                console.log("a is the greatest number");
+                            }
+                        }
+                    }
+                }
+
+            } else {
+
+                // e is greater than a
+                if (e >= b) {
+                    if (e >= c) {
+                        if (e >= d) {
+                            console.log("e is the greatest number");
+                        }
+                    }
+                }
+
+            }
+        } else {
+
+            // d is greater than a
+            if (d >= b) {
+                if (d >= c) {
+                    if (d >= e) {
+                        console.log("d is the greatest number");
+                    }
+                }
+            }
+
+        }
+    } else {
+
+        // c is greater than a
+        if (c >= b) {
+            if (c >= d) {
+                if (c >= e) {
+
+                    if (c == b) {
+                        if (c == d) {
+                            if (c == e) {
+                                console.log("b, c, d and e are equal and greatest");
+                            } else {
+                                console.log("b, c and d are equal and greatest");
+                            }
+                        } else {
+                            if (c == e) {
+                                console.log("b, c and e are equal and greatest");
+                            } else {
+                                console.log("b and c are equal and greatest");
+                            }
+                        }
+                    } else {
+                        if (c == d) {
+                            if (c == e) {
+                                console.log("c, d and e are equal and greatest");
+                            } else {
+                                console.log("c and d are equal and greatest");
+                            }
+                        } else {
+                            if (c == e) {
+                                console.log("c and e are equal and greatest");
+                            } else {
+                                console.log("c is the greatest number");
+                            }
+                        }
+                    }
+
+                } else {
+
+                    if (e >= d) {
+                        console.log("e is the greatest number");
+                    } else {
+                        console.log("d is the greatest number");
+                    }
+
+                }
+            }
+        }
+    }
+}
